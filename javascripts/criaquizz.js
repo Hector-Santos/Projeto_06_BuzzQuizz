824d15726b1b5809939daf5d00e8780d4b0ba0b7
Hector Santos <habs.404@gmail.com>
2022-04-21 15:52:36 -0300

    Criação do Quizz: Informações básicas do quizz

--- javascripts/criaquizz.js
+++ javascripts/criaquizz.js
@@ -4,22 +4,22 @@ const qntdPerguntas = "";
 const qntdNiveis = "";
 
 
-function renderScreen(button){
-if(button.classList.contains("criaQuizz")){
- document.querySelector(".container-cria-quizz").classList.remove("hidden")
-  document.querySelector(".container-cria-quizz").innerHTML =
-   `<div class="quizzTitles">Começe pelo começo</div>
+function renderScreen(button) {
+    if (button.classList.contains("criaQuizz")) {
+        document.querySelector(".container-cria-quizz").classList.remove("hidden")
+        document.querySelector(".container-cria-quizz").innerHTML =
+            `<div class="quizzTitles">Começe pelo começo</div>
    <div class="questionario">
        <input class="tituloQizz"  type="text" placeholder="Titulo do seu quiz">
        <input class="imageURL"  type="text" placeholder="URL da imagem do seu quizz">
        <input class="qtdPerguntas"  type="text" placeholder="Quantidade de perguntas do quizz">
        <input class="qtdNiveis"  type="text" placeholder="Quantidade de níveis do quizz"> 
    </div>
-   <button onclick="analisaInput()" class="criaPerguntas avancar">Prosseguir para criar perguntas</button>
+   <button onclick="analisaInput() " class="criaPerguntas avancar">Prosseguir para criar perguntas</button>
    </div>`
-}else if(button.classList.contains("criaPerguntas"))
-document.querySelector(".container-cria-quizz").innerHTML = 
-`   <span class="quizzTitles">Crie suas perguntas</span>
+    } else if (button.classList.contains("criaPerguntas"))
+        document.querySelector(".container-cria-quizz").innerHTML =
+            `   <span class="quizzTitles">Crie suas perguntas</span>
     <div class="questionario">
         <span class="quizzTitles">Pergunta 1</span>
         <input class="pergunta" type="text" placeholder="Texto da pergunta">
@@ -38,24 +38,32 @@ document.querySelector(".container-cria-quizz").innerHTML =
     <button onclick="renderScreen(this)" class="criaNiveis avancar">Prosseguir pra criar níveis</button>
 </div>`
 }
-function analisaInput(){
+function analisaInput() {
     const titulo = document.querySelector(".tituloQizz").value
-    const imagem = document.querySelector(".tituloQizz").value
+    const imagem = document.querySelector(".imageURL").value
     const perguntas = document.querySelector(".qtdPerguntas").value
-    const níveis = document.querySelector(".qtdNiveis").value
+    const niveis = document.querySelector(".qtdNiveis").value
     function isUrl(string) {
         let url;
         try {
-          url = new URL(string);
+            url = new URL(string);
         } catch (_) {
-          return false; 
+            return false;
         }
         return true
     }
-    console.log(isUrl(imagem))
+    console.log(parseInt(perguntas))
 
-    if((titulo.length > 65 || titulo.length <20) ){
-    
+    if ((titulo.length > 65 || titulo.length < 20) || !isUrl(imagem)
+        || (parseInt(perguntas) == NaN || parseInt(perguntas) < 3 )
+        || (parseInt(niveis) == NaN ||parseInt(niveis) < 2)) {
+        alert(`O título deve ter entre 20 e 65 caracteres
+A imagem deve ser um url
+O numero de perguntas deve ser um numero inteiro maior ou igual a 3
+O numero de níveis deve ser um numero inteiro maior ou igual a 2`)
+    }else{
+        renderScreen(document.querySelector(".criaPerguntas"))
+
     }
 
 }
