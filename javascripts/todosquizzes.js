3e71edb4d508a9e2f77b1f5c4942e205b28d5f2c
joaogabrielgsantos <jggsantos.25@gmail.com>
2022-04-25 04:54:34 -0300

    Exibição Todos os Quizzes e Mecanismo de resposta

--- javascripts/todosquizzes.js
+++ javascripts/todosquizzes.js
@@ -1,54 +1,94 @@
-function entrarQuizz (element) {
-    console.log ("deu certo")
+const API = "https://mock-api.driven.com.br/api/v6/buzzquizz/";
+let todosOsQuizzes;
+
+function obterQuizzes () {
+    let promise = axios.get (`${API}/quizzes`);
+    promise.then(mostrarQuizzes)
+}
+function mostrarQuizzes(response){
+    todosOsQuizzes = response.data
+    console.log (todosOsQuizzes)
+    let listaTodosQuizzes = document.querySelector(".listaTodosQuizzes")
+    for(let i = 0; i < todosOsQuizzes.length; i++){
+        listaTodosQuizzes.innerHTML += `
+                <li data-id="${todosOsQuizzes[i].id}" onclick="entrarQuizz(this)">
+                    <img src="${todosOsQuizzes[i].image}">
+                    <p>${todosOsQuizzes[i].title}</p>
+                </li>
+    `
+
+    }
+   
+}
+
+
+function entrarQuizz(element) {
+    console.log(element)
     document.querySelector(".sectionLista").classList.add("hidden");
-   const sectionExibirQuizz = document.querySelector(".sectionExibirQuizz");
-   sectionExibirQuizz.classList.remove("hidden");
-   sectionExibirQuizz.innerHTML = `
-   <div class="imagemCover">
+    const sectionExibirQuizz = document.querySelector(".sectionExibirQuizz");
+    sectionExibirQuizz.classList.remove("hidden");
+    sectionExibirQuizz.innerHTML = `
+    <div class="imagemCover">
             <img src="https://www.comboinfinito.com.br/principal/wp-content/uploads/2019/07/Yu-Gi-Oh-1.jpg">
             <div class="mask-img"></div>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-        </div>
-        <ul class="perguntasQuizz">
+         </div>
+         <ul class="perguntasQuizz">
             <li>
                 <div class="tituloPergunta">
                     <p>Possimus vero ex molestias iste temporibus sapiente?</p>
                 </div>
                 <div class="opcoesPergunta">
-                    <div>
-                        <img src="https://www.comboinfinito.com.br/principal/wp-content/uploads/2019/07/Yu-Gi-Oh-1.jpg" alt="">
+                    <div onclick="mostrarResposta(this)">
+                        <img src="https://www.comboinfinito.com.br/principal/wp-content/uploads/2019/07/Yu-Gi-Oh-1.jpg">
                         <h4>Teste</h4>
                     </div>
-                    <div>
-                        <img src="https://www.comboinfinito.com.br/principal/wp-content/uploads/2019/07/Yu-Gi-Oh-1.jpg" alt="">
+                    <div onclick="mostrarResposta(this)">
+                        <img src="https://www.comboinfinito.com.br/principal/wp-content/uploads/2019/07/Yu-Gi-Oh-1.jpg" >
                         <h4>Teste</h4>
                     </div>
-                    <div>
-                        <img src="https://www.comboinfinito.com.br/principal/wp-content/uploads/2019/07/Yu-Gi-Oh-1.jpg" alt="">
+                    <div onclick="mostrarResposta(this)">
+                        <img src="https://www.comboinfinito.com.br/principal/wp-content/uploads/2019/07/Yu-Gi-Oh-1.jpg" >
                         <h4>Teste</h4>
                     </div>
-                    <div>
-                        <img src="https://www.comboinfinito.com.br/principal/wp-content/uploads/2019/07/Yu-Gi-Oh-1.jpg" alt="">
+                    <div onclick="mostrarResposta(this)">
+                        <img src="https://www.comboinfinito.com.br/principal/wp-content/uploads/2019/07/Yu-Gi-Oh-1.jpg" >
                         <h4>Teste</h4>
                     </div>
                 </div>
             </li>
-            
-        </ul>
-        <div class="resultadoQuizz">
-            <div class="tituloResultado">
-                <p>Lorem ipsum dolor sit amet! Debitis tempore dignissimos suscipit!</p>
-            </div>
-            <div class="resultadoWrapper">
-                <img src="https://i.kym-cdn.com/photos/images/original/001/199/337/8f2.png" alt="">
-                <p>Parabéns! Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque officiis aut maiores accusamus! Eius repellendus deleniti ipsam accusantium, inventore excepturi quaerat odio eos harum repellat dolorem autem...</p>
-            </div>                
-        </div>
-        <div class="botaoReiniciarQuizz">
+         </ul>
+         <div class="botaoReiniciarQuizz">
             <h4>Reiniciar Quizz</h4>
-        </div>
-        <a href="">Voltar para home</a>   
-   
-   `
-
+         </div>
+         <a href="">Voltar para home</a>  
+    
+    
+    `;
 }
+
+function mostrarResposta(element) {
+    let listaOpcoes = document.querySelectorAll(".opcoesPergunta > div");
+    console.log(listaOpcoes)
+    for (let i = 0 ; i < listaOpcoes.length ; i++ ) {   
+        if (listaOpcoes[i].classList.contains("esbranquicado")){
+            return;
+        } else { 
+            listaOpcoes[i].classList.add("esbranquicado")
+        }
+    }
+    element.classList.add("opcaoClicada");
+}
+
+//<div class="resultadoQuizz">
+//             <div class="tituloResultado">
+//                 <p>Lorem ipsum dolor sit amet! Debitis tempore dignissimos suscipit!</p>
+//             </div>
+//             <div class="resultadoWrapper">
+//                 <img src="https://i.kym-cdn.com/photos/images/original/001/199/337/8f2.png" alt="">
+//                 <p>Parabéns! Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque officiis aut maiores accusamus! Eius repellendus deleniti ipsam accusantium, inventore excepturi quaerat odio eos harum repellat dolorem autem...</p>
+//             </div>
+//         </div>
+
+
+obterQuizzes()
